3a37fae3f6c28ca0604334069d95b9b7a8caf218
Victor Porton <porton@narod.ru>
2021-01-28 22:22:36 +0200

    initial version

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import { theme, Loader, Title } from '@gnosis.pm/safe-react-components';
+import { theme, Loader, Title, Text } from '@gnosis.pm/safe-react-components';
 import SafeProvider from '@gnosis.pm/safe-apps-react-sdk';
 
 import GlobalStyle from './GlobalStyle';
@@ -11,10 +11,12 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Text color="error" size="md">Contracts were tested, but not fully tested and not audited,. Use at your own risk!
+      NO ANY WARRANTY EVEN THE IMPLIED ONE!</Text>
       <SafeProvider
         loader={
           <>
-            <Title size="md">Waiting for Safe...</Title>
+            <Title size="md">Bequest your wallet or funds</Title>
             <Loader size="md" />
           </>
         }
