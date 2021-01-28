80649592d85456aa4e36a73b87b523d17420f658
Victor Porton <porton@narod.ru>
2021-01-28 22:55:32 +0200

    UI updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import { theme, Loader, Title, Text } from '@gnosis.pm/safe-react-components';
+import { theme, Loader, Title, Text, TextField } from '@gnosis.pm/safe-react-components';
 import SafeProvider from '@gnosis.pm/safe-apps-react-sdk';
 
 import GlobalStyle from './GlobalStyle';
@@ -11,12 +11,19 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Text color="error" size="md">Contracts were tested, but not fully tested and not audited,. Use at your own risk!
-      NO ANY WARRANTY EVEN THE IMPLIED ONE!</Text>
       <SafeProvider
         loader={
           <>
+            <Text color="error" size="md">Contracts were tested, but not fully tested and not audited,. Use at your own risk!
+            NO ANY WARRANTY EVEN THE IMPLIED ONE!</Text>
             <Title size="md">Bequest your wallet or funds</Title>
+            <Text size="lg">Your funds can be taken by the heir after:</Text>
+            <Text color="error" size="md">(Be sure to update this date periodically to ensure the heir doesn't take funds early!)</Text>
+            <TextField
+              label="The heir"
+              value=""
+            />
+            <Text size="lg">The heir can be a user account or a contract, such as another Gnosis Safe, but there is no software to take a bequest from another smart wallet yet. Surely, it will be available in the future.</Text>
             <Loader size="md" />
           </>
         }
