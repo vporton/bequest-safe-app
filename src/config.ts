7b5d68bf768de3fee2dae8f8434a5295f7dedf19
Victor Porton <porton@narod.ru>
2021-02-01 16:07:05 +0200

    BSC Testnet contract

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,10 @@ export const rpc_token = process.env.REACT_APP_RPC_TOKEN || '';
 
 export const bequestContractAddresses: {[network: string]: string} = {
     rinkeby: '0x67A212EEC9E3048bCAA0450a68Ed9cD7c01Ce4dF',
+    bsctest: '0x1B377121434Bc726a76bC0A4E3cDf021fDb02A0b',
 };
 
 export const aggregatorContractAddresses: {[network: string]: string} = {
     rinkeby: '0x5C1163Fe686354853474105eDf256C0a847f9878', // This contract is wrong, need to redeploy.
+    bsctest: '0x1B377121434Bc726a76bC0A4E3cDf021fDb02A0b', // Arbitrary address, for testing.
 };
